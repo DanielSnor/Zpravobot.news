39fa9f459d89f479721a8bcf49ec4e2f65ec4e97
DanielSnor <156429495+DanielSnor@users.noreply.github.com>
2024-01-12 12:54:11 +0100

    Create connector-ifttt-rss.ts
    v0.8.12 - the initial version on GitHub

---
+++ connector-ifttt-rss.ts
@@ -0,0 +1,10 @@
+///////////////////////////////////////////////////////////////////////////////
+// connector for IFTTT 🦋🐦‍⬛📙📘 webhook v0.8.12 - 28.12.2023
+///////////////////////////////////////////////////////////////////////////////
+const entryContent = String(Feed.newFeedItem.EntryContent);
+const entryTitle = String(Feed.newFeedItem.EntryTitle);
+const entryUrl = String(Feed.newFeedItem.EntryUrl);
+const entryImageUrl = String(Feed.newFeedItem.EntryImageUrl);
+const entryAuthor = String(Feed.newFeedItem.EntryAuthor);
+const feedTitle = String(Feed.newFeedItem.FeedTitle);
+const feedUrl = String(Feed.newFeedItem.FeedUrl);
