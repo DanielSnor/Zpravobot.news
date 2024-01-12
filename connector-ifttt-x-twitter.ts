c45c6cd4f5a2b4549e95527c8eaadd11c6a243db
DanielSnor <156429495+DanielSnor@users.noreply.github.com>
2024-01-12 12:52:58 +0100

    Create connector-ifttt-x-twitter.ts
    v0.8.12 - initial version on GitHub

---
+++ connector-ifttt-x-twitter.ts
@@ -0,0 +1,10 @@
+///////////////////////////////////////////////////////////////////////////////
+// connector for IFTTT 🐦 webhook v0.8.12 - 28.12.2023
+///////////////////////////////////////////////////////////////////////////////
+const entryContent = String(Twitter.newTweetFromSearch.Text);
+const entryTitle = String();
+const entryUrl = String(Twitter.newTweetFromSearch.LinkToTweet);
+const entryImageUrl = String(Twitter.newTweetFromSearch.UserImageUrl);
+const entryAuthor = String(Twitter.newTweetFromSearch.UserName);
+const feedTitle = String(Twitter.newTweetFromSearch.CreatedAt);
+const feedUrl = String();
