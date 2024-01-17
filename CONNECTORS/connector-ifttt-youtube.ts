6984e3dbbff6ee885d97b82107362fe03ea61bc9
Daniel Šnor <156429495+DanielSnor@users.noreply.github.com>
2024-01-17 18:05:32 +0100

    0.9.3
    date stamp and version

--- CONNECTORS/connector-ifttt-youtube.ts
+++ CONNECTORS/connector-ifttt-youtube.ts
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-// connector for IFTTT 📺 webhook v0.8.12 - 28.12.2023
+// connector for IFTTT 📺 webhook v0.9.3 - 28.1.2024
 ///////////////////////////////////////////////////////////////////////////////
 const entryContent = String(Youtube.newPublicVideoFromSubscriptions.Description);
 const entryTitle = String(Youtube.newPublicVideoFromSubscriptions.Title);
