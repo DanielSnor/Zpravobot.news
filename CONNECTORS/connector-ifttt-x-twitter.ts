759cbc5494b6356c920fff4d570f3a220388e609
Daniel Šnor E.ON <d33439@seanomacbook.local>
2024-02-16 13:25:02 +0100

    Change resulted from Nitter’s end Fix for TW part of script

--- CONNECTORS/connector-ifttt-x-twitter.ts
+++ CONNECTORS/connector-ifttt-x-twitter.ts
@@ -1,10 +1,10 @@
 ///////////////////////////////////////////////////////////////////////////////
-// connector for IFTTT 🐦‍⬛ webhook v0.9.3 - 17.1.2024
+// connector for IFTTT 🐦‍⬛ webhook v0.9.4 - 16.2.2024
 ///////////////////////////////////////////////////////////////////////////////
 const entryContent = String(Twitter.newTweetFromSearch.Text);
-const entryTitle = String();
+const entryTitle = String(Twitter.newTweetFromSearch.Text);
 const entryUrl = String(Twitter.newTweetFromSearch.LinkToTweet);
 const entryImageUrl = String(Twitter.newTweetFromSearch.UserImageUrl);
 const entryAuthor = String(Twitter.newTweetFromSearch.UserName);
-const feedTitle = String(Twitter.newTweetFromSearch.CreatedAt);
+const feedTitle = String(Twitter.newTweetFromSearch.UserName);
 const feedUrl = String();
