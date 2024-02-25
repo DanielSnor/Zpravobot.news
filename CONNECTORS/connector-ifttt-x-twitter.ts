6328d14f2292e9fffbca8433a3f2a3460286e91d
Daniel Šnor <d33439@seanomacbook.local>
2024-02-25 09:16:55 +0100

    updated connector for Twitter

--- CONNECTORS/connector-ifttt-x-twitter.ts
+++ CONNECTORS/connector-ifttt-x-twitter.ts
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-// connector for IFTTT 🐦‍⬛ webhook - 17.2.2024
+// connector for IFTTT 🐦‍⬛ webhook - 24.2.2024
 ///////////////////////////////////////////////////////////////////////////////
 const entryContent = String(Twitter.newTweetFromSearch.Text);
 const entryTitle = String(Twitter.newTweetFromSearch.Text);
@@ -7,4 +7,4 @@ const entryUrl = String(Twitter.newTweetFromSearch.LinkToTweet);
 const entryImageUrl = String(Twitter.newTweetFromSearch.UserImageUrl);
 const entryAuthor = String(Twitter.newTweetFromSearch.UserName);
 const feedTitle = String(Twitter.newTweetFromSearch.UserName);
-const feedUrl = String();
+const feedUrl = String("https://twitter.com/" + Twitter.newTweetFromSearch.UserName);
