6745b7c017ceade3324965b434b32a41ffaf7875
Daniel Šnor <156429495+DanielSnor@users.noreply.github.com>
2025-01-02 21:18:06 +0100

    Update settings-ifttt-bluesky.ts
    mods for cooperation with the last changes for BS

--- SETTINGS/settings-ifttt-bluesky.ts
+++ SETTINGS/settings-ifttt-bluesky.ts
@@ -1,21 +1,21 @@
 ///////////////////////////////////////////////////////////////////////////////
-// settings for IFTTT 🦋 webhook filter - 17.2.2024
+// settings for IFTTT 🦋 webhook filter - 2.1.2025
 ///////////////////////////////////////////////////////////////////////////////
 const SETTINGS = {
-  AMPERSAND_REPLACEMENT: ` a `, // replacement for & char
+  AMPERSAND_REPLACEMENT: `a`, // replacement for & char
   COMMERCIAL_SENTENCE: "", // "" | "Komerční sdělení:"
-  POST_FROM: "BS", // "BS" | "NT" | "RSS" | "TW" | "YT"
-  POST_LENGTH: 4750, // 0 - 5000 chars
-  POST_SOURCE: "", // "" | `https://nitter.cz/` | `https://twitter.com/`
-  POST_TARGET: "", //  "" | `https://nitter.cz/` | `https://twitter.com/`
+  POST_FROM: "BS", // "BS" | "RSS" | "TW" | "YT"
+  POST_LENGTH: 475, // 0 - 5000 chars
+  POST_SOURCE: "", // "" | `https://twitter.com/`
+  POST_TARGET: "", //  "" | `https://twitter.com/`
   USER_INSTANCE: ".bsky.social", // "" | ".bsky.social" | "instagram.com" | "twitter.com" | "x.com" | "youtube.com"
-  QUOTE_SENTENCE: "📝💬🦋", // "" | "komentoval příspěvek od" | "📝💬🦋" | "📝💬🐦‍⬛"
-  REPOST_ALLOWED: true, // true | false
-  REPOST_SENTENCE: "📤🦋", // "" | "sdílí" | "📤🦋" | "📤🐦‍⬛"
+  QUOTE_SENTENCE: "contains quote post or other embedded content", // "" | "komentoval příspěvek od" | "contains quote post or other embedded content" | "📝💬🐦‍⬛"
+  REPOST_ALLOWED: false, // true | false
+  REPOST_SENTENCE: "", // "" | "sdílí" | "📤🐦‍⬛"
   SHOULD_PREFER_REAL_NAME: true, // true | false
   SHOW_FEEDURL_INSTD_POSTURL: false, // true | false
   SHOW_IMAGEURL: false, // true | false
-  SHOW_ORIGIN_POSTURL_PERM: true, // true | false
+  SHOW_ORIGIN_POSTURL_PERM: false, // true | false
   STATUS_IMAGEURL_SENTENCE: "", // "" | "🖼️"
   STATUS_URL_SENTENCE: "", // "" | "🔗" | "\n🗣️🎙️👇👇👇\n" | "\nYT 📺👇👇👇\n"
 };
