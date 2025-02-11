60f4b47be3b6a664aad117707c984590b1ef2ce3
Daniel Šnor <156429495+DanielSnor@users.noreply.github.com>
2025-02-11 09:37:00 +0100

    Update settings-ifttt-bluesky.ts

--- SETTINGS/settings-ifttt-bluesky.ts
+++ SETTINGS/settings-ifttt-bluesky.ts
@@ -1,23 +1,23 @@
 ///////////////////////////////////////////////////////////////////////////////
-// settings for IFTTT 🦋 webhook filter - 2.1.2025
+// settings for IFTTT 🦋 webhook filter - rev 15.2.2025
 ///////////////////////////////////////////////////////////////////////////////
 const SETTINGS = {
   AMPERSAND_REPLACEMENT: `a`, // replacement for & char
   COMMERCIAL_SENTENCE: "", // "" | "Komerční sdělení:"
   POST_FROM: "BS", // "BS" | "RSS" | "TW" | "YT"
-  POST_LENGTH: 475, // 0 - 5000 chars
+  POST_LENGTH: 400, // 0 - 5000 chars
   POST_SOURCE: "", // "" | `https://twitter.com/`
-  POST_TARGET: "", //  "" | `https://twitter.com/`
-  USER_INSTANCE: ".bsky.social", // "" | ".bsky.social" | "instagram.com" | "twitter.com" | "x.com" | "youtube.com"
-  QUOTE_SENTENCE: "contains quote post or other embedded content", // "" | "komentoval příspěvek od" | "contains quote post or other embedded content" | "📝💬🐦‍⬛"
+  POST_TARGET: "", //  "" | `https://x.com/`
+  USER_INSTANCE: ".bsky.social", // "" | ".bsky.social" | "@twitter.com" | "@x.com"
+  QUOTE_SENTENCE: "contains quote post or other embedded content", // "" | "contains quote post or other embedded content" | "𝕏📝💬"
   REPOST_ALLOWED: false, // true | false
-  REPOST_SENTENCE: "", // "" | "sdílí" | "📤🐦‍⬛"
+  REPOST_SENTENCE: "", // "" | "sdílí" | "𝕏📤"
   SHOULD_PREFER_REAL_NAME: true, // true | false
   SHOW_FEEDURL_INSTD_POSTURL: false, // true | false
   SHOW_IMAGEURL: false, // true | false
-  SHOW_ORIGIN_POSTURL_PERM: false, // true | false
+  SHOW_ORIGIN_POSTURL_PERM: true, // true | false
   STATUS_IMAGEURL_SENTENCE: "", // "" | "🖼️"
-  STATUS_URL_SENTENCE: "", // "" | "🔗" | "\n🗣️🎙️👇👇👇\n" | "\nYT 📺👇👇👇\n"
+  STATUS_URL_SENTENCE: "\n", // "" | "🔗" | "\n🗣️🎙️👇👇👇\n" | "\nYT 📺👇👇👇\n"
 };
 
 // content hack - replace ZZZZZ and KKKKK with the beginning and the end of content designated to remove
