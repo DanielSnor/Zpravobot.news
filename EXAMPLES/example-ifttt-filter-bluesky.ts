5ddea8747e3d75fbd77a2699491e69611619876e
Daniel Šnor <156429495+DanielSnor@users.noreply.github.com>
2025-02-25 08:41:48 +0100

    Update example-ifttt-filter-bluesky.ts

--- EXAMPLES/example-ifttt-filter-bluesky.ts
+++ EXAMPLES/example-ifttt-filter-bluesky.ts
@@ -52,7 +52,7 @@ function contentHack(str: string): string {
 ///////////////////////////////////////////////////////////////////////////////
 const entryContent = String(Feed.newFeedItem.EntryContent); // post content
 const entryTitle = String(Feed.newFeedItem.EntryTitle); // post title
-const entryUrl = String(Feed.newFeedItem.EntryUrl); // link to the tweet
+const entryUrl = String(Feed.newFeedItem.EntryUrl); // link to the post
 const entryImageUrl = String(Feed.newFeedItem.EntryImageUrl); // URL to the user image
 const entryAuthor = String(Feed.newFeedItem.EntryAuthor); // author's username
 const feedTitle = String(Feed.newFeedItem.FeedTitle); // title of the feed (username)
