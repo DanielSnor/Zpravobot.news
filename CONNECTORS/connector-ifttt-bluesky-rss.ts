13ec7ae5adfe372122b15fc58cc404b84e50ee49
Daniel Šnor <156429495+DanielSnor@users.noreply.github.com>
2025-03-13 21:01:10 +0100

    Update connector-ifttt-bluesky-rss.ts Pi day rev

--- CONNECTORS/connector-ifttt-bluesky-rss.ts
+++ CONNECTORS/connector-ifttt-bluesky-rss.ts
@@ -1,6 +1,23 @@
 ///////////////////////////////////////////////////////////////////////////////
-// connector for IFTTT 🦋📙📗📘 webhook - rev 5.3.2025
+// connector for IFTTT 🦋📙📗📘 webhook - Pi Day 2025 rev
 ///////////////////////////////////////////////////////////////////////////////
+// 
+// This connector processes data from various sources (e.g. RSS, Twitter, Bluesky) 
+// and provides it to the IFTTT webhook for publishing.
+// 
+// The data is filtered and edited according to the settings in AppSettings.
+// 
+// Sources:
+// - entryContent: Entry content for post
+// - entryTitle: Entry title for post
+// - entryUrl: Post URL
+// - entryImageUrl: URL of the first image in the post
+// - entryAuthor: Name of the author of the post
+// - feedTitle: Feed Title (username)
+// - feedUrl: Feed URL
+// 
+///////////////////////////////////////////////////////////////////////////////
+
 const entryContent = String(Feed.newFeedItem.EntryContent); // post content
 const entryTitle = String(Feed.newFeedItem.EntryTitle); // post title
 const entryUrl = String(Feed.newFeedItem.EntryUrl); // link to the post
