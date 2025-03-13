3e8218778e76ba97be821ea1d8633cab80d7427c
Daniel Šnor <156429495+DanielSnor@users.noreply.github.com>
2025-03-13 21:02:20 +0100

    Update connector-ifttt-youtube.ts Pi day rev

--- CONNECTORS/connector-ifttt-youtube.ts
+++ CONNECTORS/connector-ifttt-youtube.ts
@@ -1,6 +1,23 @@
 ///////////////////////////////////////////////////////////////////////////////
-// connector for IFTTT 📺 webhook - rev 5.3.2025
+// connector for IFTTT 📺 webhook - Pi Day 2025 rev
 ///////////////////////////////////////////////////////////////////////////////
+// 
+// This connector processes data from various sources (e.g. RSS, Twitter, Bluesky) 
+// and provides it to the IFTTT webhook for publishing.
+// 
+// The data is filtered and edited according to the settings in AppSettings.
+// 
+// Sources:
+// - entryContent: Entry content for post
+// - entryTitle: Entry title for post
+// - entryUrl: Post URL
+// - entryImageUrl: URL of the first image in the post
+// - entryAuthor: Name of the author of the post
+// - feedTitle: Feed Title (username)
+// - feedUrl: Feed URL
+// 
+///////////////////////////////////////////////////////////////////////////////
+
 const entryContent = String(Youtube.newPublicVideoFromSubscriptions.Description); // post content
 const entryTitle = String(Youtube.newPublicVideoFromSubscriptions.Title); // post title
 const entryUrl = String(Youtube.newPublicVideoFromSubscriptions.Url); // link to the post
