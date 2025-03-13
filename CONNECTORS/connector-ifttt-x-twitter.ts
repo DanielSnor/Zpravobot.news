2dce1dba895754790a06cb839d0ed23f8748114f
Daniel Šnor <156429495+DanielSnor@users.noreply.github.com>
2025-03-13 21:01:44 +0100

    Update connector-ifttt-x-twitter.ts Pi day rev

--- CONNECTORS/connector-ifttt-x-twitter.ts
+++ CONNECTORS/connector-ifttt-x-twitter.ts
@@ -1,6 +1,23 @@
 ///////////////////////////////////////////////////////////////////////////////
-// connector for IFTTT 𝕏 webhook - rev 5.3.2025
+// connector for IFTTT 𝕏 webhook - Pi Day 2025 rev
 ///////////////////////////////////////////////////////////////////////////////
+// 
+// This connector processes data from various sources (e.g. RSS, Twitter, Bluesky) 
+// and provides it to the IFTTT webhook for publishing.
+// 
+// The data is filtered and edited according to the settings in AppSettings.
+// 
+// Sources:
+// - entryContent: Entry content for post
+// - entryTitle: Entry title for post
+// - entryUrl: Post URL
+// - entryImageUrl: URL of the first image in the post
+// - entryAuthor: Name of the author of the post
+// - feedTitle: Feed Title (username)
+// - feedUrl: Feed URL
+// 
+///////////////////////////////////////////////////////////////////////////////
+
 const entryContent = String(Twitter.newTweetFromSearch.Text); // tweet content
 const entryTitle = String(Twitter.newTweetFromSearch.Text); // tweet title
 const entryUrl = String(Twitter.newTweetFromSearch.LinkToTweet); // link to the tweet
