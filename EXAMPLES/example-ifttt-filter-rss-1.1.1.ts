ed844d15f2a5c0acb957ce441cec6be0dc1447b9
Daniel Šnor <156429495+DanielSnor@users.noreply.github.com>
2025-03-14 10:10:37 +0100

    Last moment getContent() fix

--- EXAMPLES/example-ifttt-filter-rss-1.1.1.ts
+++ EXAMPLES/example-ifttt-filter-rss-1.1.1.ts
@@ -241,8 +241,8 @@ function getContent(entryContent: any, entryTitle: any): string {
   if (SETTINGS.SHOW_TITLE_AS_CONTENT) {
     return entryTitle || "";
   } else {
-    // returns content from entryContent, or entryTitle if entryContent is empty
-    return entryContent || entryTitle || "";
+    // returns content from entryContent, or entryTitle if entryContent is empty or contains only whitespace
+    return (typeof entryContent === "string" && entryContent.trim().length === 0) || entryContent === "(none)" ? entryTitle : entryContent;
   }
 }
 
