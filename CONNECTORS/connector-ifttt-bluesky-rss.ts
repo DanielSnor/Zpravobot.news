e8deb165ec74ed5e4b0b1ef98940341bfc1c1a98
Daniel Šnor <156429495+DanielSnor@users.noreply.github.com>
2025-07-16 13:00:44 +0200

    v2.0.0 - World Emoji day, 2025 rev
    Update connector-ifttt-bluesky-rss.ts

--- CONNECTORS/connector-ifttt-bluesky-rss.ts
+++ CONNECTORS/connector-ifttt-bluesky-rss.ts
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////////////
-// connector for IFTTT 🦋📙📗📘 webhook - World Juggling day, 2025 rev
+// connector for IFTTT 🦋📙📗📘 webhook - World Emoji day, 2025 rev
 ///////////////////////////////////////////////////////////////////////////////
 //
 // This connector processes data from various sources (e.g., RSS, Twitter, Bluesky)
