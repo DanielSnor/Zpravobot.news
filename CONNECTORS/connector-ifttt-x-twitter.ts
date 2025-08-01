0f46c7cb203dc9f0395bfbc6e326e4f61a025d6a
Daniel Šnor <156429495+DanielSnor@users.noreply.github.com>
2025-08-01 19:12:09 +0200

    Update connector-ifttt-x-twitter.ts
    v2.0.2 - Velcro Day, Aug 2nd, 2025 rev

--- CONNECTORS/connector-ifttt-x-twitter.ts
+++ CONNECTORS/connector-ifttt-x-twitter.ts
@@ -1,24 +1,24 @@
 ///////////////////////////////////////////////////////////////////////////////
-// Connector for IFTTT 𝕏 webhook - World Emoji day, 2025 rev
+// Connector for IFTTT 𝕏 webhook - Velcro Day, Aug 2nd, 2025 rev
 ///////////////////////////////////////////////////////////////////////////////
 //
 // This connector processes data from various sources (e.g., RSS, Twitter, Bluesky)
 // and provides it to the IFTTT webhook for publishing. The data is filtered and
-// edited according to the settings in AppSettings.v2.0.0 - World Emoji day, 2025 rev
+// edited according to the settings in AppSettings.
 //
 ///////////////////////////////////////////////////////////////////////////////
 
 // Main text content from the source. For Twitter, this is often TweetEmbedCode (HTML embed code).
-const entryContent = Twitter.newTweetFromSearch.TweetEmbedCode || '';
+const entryContent = Twitter.newTweetFromSearch.TweetEmbedCode || "";
 // Title from the source. For Twitter, this is clean content without HTML (Text field).
-const entryTitle = Twitter.newTweetFromSearch.Text || '';
+const entryTitle = Twitter.newTweetFromSearch.Text || "";
 // URL of the specific post/item. For Twitter, this is the direct link to the tweet.
-const entryUrl = Twitter.newTweetFromSearch.LinkToTweet || '';
+const entryUrl = Twitter.newTweetFromSearch.LinkToTweet || "";
 // URL of the first image/media link found in the post. For Twitter, this is FirstLinkUrl.
-const entryImageUrl = Twitter.newTweetFromSearch.FirstLinkUrl || '';
+const entryImageUrl = Twitter.newTweetFromSearch.FirstLinkUrl || "";
 // Username of the post author. For Twitter, this is the UserName field.
-const entryAuthor = Twitter.newTweetFromSearch.UserName || '';
+const entryAuthor = Twitter.newTweetFromSearch.UserName || "";
 // Title of the feed (can be username, feed name, etc.). For Twitter, this is often UserName.
-const feedTitle = Twitter.newTweetFromSearch.UserName || '';
+const feedTitle = Twitter.newTweetFromSearch.UserName || "";
 // URL of the source feed/profile. For Twitter, this is constructed from the username.
-const feedUrl = "https://twitter.com/" + (Twitter.newTweetFromSearch.UserName || '');
+const feedUrl = "https://twitter.com/" + (Twitter.newTweetFromSearch.UserName || "");
